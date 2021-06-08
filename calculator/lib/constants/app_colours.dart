a568dd6e73a102db12c1b2e95f8b45cf25405442
Ntobeko Dindi <xcoding101@gmail.com>
2021-06-08 19:44:09 +0200

    calculator UI button design

---
+++ calculator/lib/constants/app_colours.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppColours {
+  AppColours._();
+
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFF808080);
+  static const Color orange = Color(0xFFFFA500);
+  static const Color darkerOrage = Color(0xFFFF8C00);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color red = Color(0xFFFF0000);
+}
